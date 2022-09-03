9d6e47d954f311bb8fccf95dda85fa09ffb032b9
CB-Rodrigo-Martins <rodrigo.martins@cuboconnect.com>
2022-09-03 19:32:02 -0300

    DATABASE creation

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Rodrigo Martins', 100, 90, 9999.9, '2022-09-03');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Charizard', 5, 50, 6000.9, '2021-01-03');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Blastoise', 8, 10, 100.9, '2021-05-08');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Venussaur', 1000, 60, 65000.9, '2021-12-13');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Mewtwo', 150, 150, 150.0, '2021-01-03');
