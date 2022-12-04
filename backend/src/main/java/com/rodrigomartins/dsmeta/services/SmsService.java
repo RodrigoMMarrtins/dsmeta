1e2f8a25a4664ce435a25a02985c20118ef65910
CB-Rodrigo-Martins <rodrigo.martins@cuboconnect.com>
2022-12-04 14:53:59 -0300

    SMS method with id

--- backend/src/main/java/com/rodrigomartins/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/rodrigomartins/dsmeta/services/SmsService.java
@@ -1,7 +1,10 @@
 package com.rodrigomartins.dsmeta.services;
 
+import com.rodrigomartins.dsmeta.entities.Sale;
+import com.rodrigomartins.dsmeta.repository.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.twilio.rest.api.v2010.account.Message;
@@ -21,13 +24,23 @@ public class SmsService {
     @Value("${twilio.phone.to}")
     private String twilioPhoneTo;
 
-    public void sendSms() {
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public void sendSms(Long id) {
+        Sale sale = saleRepository.findById(id).get();
+
+        String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+
+        String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+                + " com um total de R$ " + String.format("%.2f", sale.getAmount());
+
         Twilio.init(twilioSid, twilioKey);
 
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
         PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-        Message message = Message.creator(to, from, "Te amooooooo").create();
+        Message message = Message.creator(to, from, msg).create();
 
         System.out.println(message.getSid());
     }
