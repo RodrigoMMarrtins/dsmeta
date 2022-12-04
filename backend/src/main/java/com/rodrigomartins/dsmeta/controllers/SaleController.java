90b2609ea67961459f74aa51839c82969e2b14b1
CB-Rodrigo-Martins <rodrigo.martins@cuboconnect.com>
2022-12-04 14:25:55 -0300

    SMS method

--- backend/src/main/java/com/rodrigomartins/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/rodrigomartins/dsmeta/controllers/SaleController.java
@@ -3,6 +3,7 @@ package com.rodrigomartins.dsmeta.controllers;
 import com.rodrigomartins.dsmeta.entities.Sale;
 import com.rodrigomartins.dsmeta.services.SaleService;
 
+import com.rodrigomartins.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/notification")
+    public void notifySms() {
+        smsService.sendSms();
     }
 }
