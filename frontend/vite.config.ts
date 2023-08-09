3c376ec9fd5e3da17ac86bd6aad0424af4ffed1a
RodrigoMMarrtins <rodrigomendesmartins22@gmail.com>
2023-08-09 10:19:04 -0300

    Configuracao do docker do frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+    strictPort: true,
+    port: 5173,
+  },
+  
 })
